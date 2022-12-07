f78793553a995cf7df644812bcbb5f8b4c313850
Amund Kulsrud Storruste <amund.kulsrud.storruste@kongsbergdigital.com>
2022-12-07 14:58:00 +0100

    cleanup

--- 07.12.2022/main.py
+++ 07.12.2022/main.py
@@ -2,47 +2,30 @@ def do_recursive_jumbo(filereader, dir_sizes):
     dir_size = 0
 
     while True:
-        # find dir_size
         line = filereader.readline()
 
-        if line == "":
-            break
+        # Stop condition
+        if line == "$ cd ..\n" or line == "":
+            dir_sizes.append(dir_size)
+            return dir_size
 
+        # Add subdirectory size to directory size
         if line.split()[1] == "ls":
-            sub_folder_size = do_recursive_jumbo(filereader, dir_sizes)
-            dir_size+= sub_folder_size
-        # Else we have either a dir or a filesize
+            dir_size+= do_recursive_jumbo(filereader, dir_sizes)
         
-        if line.split()[0] != "$" and line.split()[0] != "dir":
+        # Add local files to directory size
+        if line.split()[0].isdigit():
             dir_size += int(line.split()[0])
         
-        if line == "$ cd ..\n":
-            break
-    dir_sizes.append(dir_size)
-    return dir_size
-
 
 with open('puzzle_input.txt') as filereader:
-    # lines = f.readlines()
-
-    # Find size of all dictionaries
-    isCounting = False
     dir_sizes = []
-    while (True):
-        line = filereader.readline()
-        if line == "":
-            break
-        if line.split()[1] == "ls":
-            do_recursive_jumbo(filereader, dir_sizes)
+    do_recursive_jumbo(filereader, dir_sizes)
 
-    print("done")
+    # Add up directories smaller than 100000
+    task_a = sum(i for i in dir_sizes if i <= 100000)
+    print(f"Task a): {task_a}")
 
-    # Add up
-    res = sum(i for i in dir_sizes if i <= 100000)
-    print(res)
-
-    total_size = sum(dir_sizes)
-    print(total_size)
 
 # Identify availible space
 with open('puzzle_input.txt') as filereader:
@@ -51,18 +34,16 @@ with open('puzzle_input.txt') as filereader:
         line = filereader.readline()
         if line == "":
             break
-        if line.split()[0] != "$" and line.split()[0] != "dir":
+        if line.split()[0].isdigit():
             filesize += int(line.split()[0])
-
-    print(f"filesize: {filesize}")
 
 filesystem = 70000000
 required_space = 30000000
 
-space = filesystem - filesize
-required_deletion  = required_space - space
+availible_space = filesystem - filesize
+missing_space  = required_space - availible_space
 
-potential_dirs = [i for i in dir_sizes if i >= required_deletion]
-best_option = min(potential_dirs)
+potential_dirs = [i for i in dir_sizes if i >= missing_space]
+task_b = min(potential_dirs)
 
-print(best_option)
+print(f"Task b): {task_b}")
